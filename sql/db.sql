9ccc4a5ce53af79765f23b8a1d5d4bc566cb2850
Bryan Monterrosa <bryan_monterrosa@hotmail.com>
2025-10-11 00:25:59 -0600

    Primer commit. Proyecto maven con Log4J2 configurado.

---
+++ sql/db.sql
@@ -0,0 +1,83 @@
+DROP DATABASE IF EXISTS mediateca;
+CREATE DATABASE mediateca;
+USE mediateca;
+
+-- Tabla principal
+CREATE TABLE material (
+                          id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+                          codigo_interno VARCHAR(16) NOT NULL,
+                          tipo ENUM('LIB','REV','CDA','DVD') NOT NULL,
+                          titulo VARCHAR(255) NOT NULL,
+                          unidades_disponibles INT NOT NULL DEFAULT 0,
+                          CONSTRAINT pk_material PRIMARY KEY (id),
+                          CONSTRAINT uq_material_codigo UNIQUE (codigo_interno)
+);
+
+CREATE TABLE seq_tipo (
+                          tipo ENUM('LIB','REV','CDA','DVD') PRIMARY KEY,
+                          siguiente INT NOT NULL
+);
+
+INSERT INTO seq_tipo VALUES ('LIB',0),('REV',0),('CDA',0),('DVD',0);
+
+DELIMITER //
+CREATE TRIGGER trg_material_bi
+    BEFORE INSERT ON material
+    FOR EACH ROW
+BEGIN
+    UPDATE seq_tipo
+    SET siguiente = LAST_INSERT_ID(siguiente + 1)
+    WHERE tipo = NEW.tipo;
+    SET NEW.codigo_interno = CONCAT(NEW.tipo, LPAD(LAST_INSERT_ID(), 5, '0'));
+END//
+DELIMITER ;
+
+-- Subtabla: LIBRO
+CREATE TABLE libro (
+                       id_material BIGINT UNSIGNED NOT NULL,
+                       autor VARCHAR(255) NOT NULL,
+                       numero_paginas INT NOT NULL,
+                       editorial VARCHAR(255) NOT NULL,
+                       isbn VARCHAR(20) NOT NULL,
+                       anio_publicacion YEAR NOT NULL,
+
+                       CONSTRAINT pk_libro PRIMARY KEY (id_material),
+                       CONSTRAINT fk_libro_material
+                           FOREIGN KEY (id_material) REFERENCES material(id)
+);
+
+-- Subtabla: REVISTA
+CREATE TABLE revista (
+                         id_material BIGINT UNSIGNED NOT NULL,
+                         editorial VARCHAR(255) NOT NULL,
+                         periodicidad VARCHAR(50) NOT NULL,
+                         fecha_publicacion DATE NOT NULL,
+
+                         CONSTRAINT pk_revista PRIMARY KEY (id_material),
+                         CONSTRAINT fk_revista_material
+                             FOREIGN KEY (id_material) REFERENCES material(id)
+);
+
+-- Subtabla: CD DE AUDIO
+CREATE TABLE cd_audio (
+                          id_material BIGINT UNSIGNED NOT NULL,
+                          artista VARCHAR(255) NOT NULL,
+                          genero VARCHAR(100) NOT NULL,
+                          duracion_min INT NOT NULL,
+                          numero_canciones INT NOT NULL,
+                          CONSTRAINT pk_cd_audio PRIMARY KEY (id_material),
+                          CONSTRAINT fk_cd_audio_material
+                              FOREIGN KEY (id_material) REFERENCES material(id)
+);
+
+-- Subtabla: DVD
+CREATE TABLE dvd (
+                     id_material BIGINT UNSIGNED NOT NULL,
+                     director VARCHAR(255) NOT NULL,
+                     genero VARCHAR(100) NOT NULL,
+                     duracion_min INT NOT NULL,
+                     CONSTRAINT pk_dvd PRIMARY KEY (id_material),
+                     CONSTRAINT fk_dvd_material
+                         FOREIGN KEY (id_material) REFERENCES material(id)
+
+);
