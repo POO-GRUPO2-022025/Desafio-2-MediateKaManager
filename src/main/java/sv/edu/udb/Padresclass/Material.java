2d244ef75449f3999b29dc8688c7b40b0ed574fb
Ronald Vargas <vargas21miguel@gmail.com>
2025-10-23 20:58:32 -0600

    Agregandoclass (#1)
    * se creo las clases padres e hijas
    
    * creacion de clases hijas
    
    * Se agrego la padreclass
    
    * Cambios en padres class y hijasclass
    
    * modificacion revista
    
    * Seagregaron HIjas Class
    
    * agregando material y libro
    
    * agrenado clases
    
    * agregando class hijas

---
+++ src/main/java/sv/edu/udb/Padresclass/Material.java
@@ -0,0 +1,85 @@
+package sv.edu.udb.Padresclass;
+
+public class Material {
+
+    // Enum para los tipos de material
+    public enum TipoMaterial {
+        LIBRO,
+        REVISTA,
+        CD,
+        DVD
+    }
+
+    //  Atributos comunes
+    private long id;
+    private String codigoInterno;
+    private String titulo;
+    private int unidadesDisponibles;
+    private TipoMaterial tipoMaterial; // añadimos un tipo para identificarlo
+
+    //  Constructor
+    public Material(long id, String codigoInterno, String titulo, int unidadesDisponibles, TipoMaterial tipoMaterial) {
+        this.id = id;
+        this.codigoInterno = codigoInterno;
+        this.titulo = titulo;
+        this.unidadesDisponibles = unidadesDisponibles;
+        this.tipoMaterial = tipoMaterial;
+    }
+
+    //  Getters y Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigoInterno() {
+        return codigoInterno;
+    }
+
+    public void setCodigoInterno(String codigoInterno) {
+        this.codigoInterno = codigoInterno;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getUnidadesDisponibles() {
+        return unidadesDisponibles;
+    }
+
+    public void setUnidadesDisponibles(int unidadesDisponibles) {
+        this.unidadesDisponibles = unidadesDisponibles;
+    }
+
+    public TipoMaterial getTipoMaterial() {
+        return tipoMaterial;
+    }
+
+    public void setTipoMaterial(TipoMaterial tipoMaterial) {
+        this.tipoMaterial = tipoMaterial;
+    }
+
+    //  Método para mostrar información
+    public String mostrarInformacion() {
+        return "ID: " + id +
+               ", Código: " + codigoInterno +
+               ", Título: " + titulo +
+               ", Unidades disponibles: " + unidadesDisponibles +
+               ", Tipo: " + tipoMaterial;
+    }
+
+    //  Sobrescritura del método toString()
+    @Override
+    public String toString() {
+        return mostrarInformacion();
+    }
+}
+
